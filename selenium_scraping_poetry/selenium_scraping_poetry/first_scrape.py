c45223e52036337eca925378c196f9d4efbf3d7a
Tung <tnguyen1@iwu.edu>
2024-08-03 16:31:36 -0400

    modify scripts to work across laptops

--- selenium_scraping_poetry/selenium_scraping_poetry/first_scrape.py
+++ selenium_scraping_poetry/selenium_scraping_poetry/first_scrape.py
@@ -4,6 +4,8 @@ from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 from loguru import logger
 from bs4 import BeautifulSoup
+from pathlib import Path
+file_path = Path(__file__).resolve().parent
 
 def main():
     # Set up options for headless Chrome
@@ -15,11 +17,20 @@ def main():
     # options.add_argument("--window-size=1920,1200")  # Define the window size of the browser
 
     # Set the path to the Chromedriver
-    DRIVER_PATH = r'D:\personal_projects\selenium_scraping\data\Driver_Notes_edge\msedgedriver.exe'
+
+    DRIVER_PATH = file_path.parent.parent / "data" / "laptop" / "msedgedriver.exe"
+    # "/mnt/c/Users/tnguy/personal_projects/selenium_scraping/selenium_webscraping/data/Driver_Notes_edge/msedgedriver.exe"
+    # DRIVER_PATH = r"C:\Users\tnguy\personal_projects\selenium_scraping\selenium_webscraping\data\Driver_Notes_edge\msedgedriver.exe"
+    # logger.info(f"Current driver path: {DRIVER_PATH}")
+    # return
 
     # Initialize Chrome with the specified options
-    edge_service = Service(executable_path=DRIVER_PATH)
-    driver = webdriver.Edge(service=edge_service, options=edge_options)
+    # edge_service = Service(executable_path=DRIVER_PATH, verbose=True)
+    # # service = Service(verbose = True)
+    driver = webdriver.Edge(options=edge_options)
+    # service = Service()
+    # options = webdriver.ChromeOptions()
+    # driver = webdriver.Chrome(service=service, options=options)
 
     # Navigate to the Nintendo website
     # driver.get("https://www.kroger.com/")
@@ -36,27 +47,28 @@ def main():
     # logger.info(f"Html: {html}")
     # logger.info(f"Soup: {soup}")
     # logger.info(f"Titles: {titles}")
-    driver.get("https://www.instacart.com/store/publix/storefront")
-
+    driver.get("https://www.brainyquote.com/")
+    # search = driver.find_element_by_xpath("//div[@class='input-container']")
+    # logger.info(search)
     # Retrieve the page source
-    html = driver.page_source
+    # html = driver.page_source
 
-    # Close the driver
-    driver.quit()
+    # # Close the driver
+    # driver.quit()
 
-    # Parse the HTML with BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
+    # # Parse the HTML with BeautifulSoup
+    # soup = BeautifulSoup(html, 'html.parser')
 
-    # Find all 'tr' elements with class 'athing' which contain the news titles
-    titles = soup.find_all('tr', class_='athing')
+    # # Find all 'tr' elements with class 'athing' which contain the news titles
+    # titles = soup.find_all('tr', class_='athing')
 
-    # # Loop through each title and print it
-    # for title in titles:
-    #     # Find the <a> tag within the 'titleline' span inside a 'td' with class 'title'
-    #     title_link = title.find('td', class_='title').find('span', class_='titleline').find('a')
-    #     title_text = title_link.get_text()  # Extract the text of the title
-    #     print(title_text)
-    logger.info(f"Titles: {soup}")
+    # # # Loop through each title and print it
+    # # for title in titles:
+    # #     # Find the <a> tag within the 'titleline' span inside a 'td' with class 'title'
+    # #     title_link = title.find('td', class_='title').find('span', class_='titleline').find('a')
+    # #     title_text = title_link.get_text()  # Extract the text of the title
+    # #     print(title_text)
+    # logger.info(f"Titles: {soup}")
 
 if __name__ == "__main__":
     main()
